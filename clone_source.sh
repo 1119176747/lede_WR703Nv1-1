2c7531862a3f3492f28ecc4d3dd76a66a6b124ce
1119176747 <126279546+1119176747@users.noreply.github.com>
2023-05-23 01:28:04 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -9,7 +9,7 @@ sed -i -e 's/192.168.1.1/192.168.1.11/g' -e 's/OpenWrt/TPLink_WR703Nv1/g' packag
 sed -i '343s/4/16/g' target/linux/ar71xx/image/tiny-tp-link.mk  #修改wr703nv1的编译固件大小
 sed -i -e 's/OpenWrt/TPLink_WR703Nv1/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 修改WiFi名称,添加加密方式和密码
-
+echo 'src-git passwall https://github.com/songchenwen/openwrt-package.git' >>feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default                                      #使用源码自带ShadowSocksR Plus软件
 sed -i -e '1s/$/^e82d730/g' feeds.conf.default
 sed -i -e '2s/$/^974fb04/g' feeds.conf.default
